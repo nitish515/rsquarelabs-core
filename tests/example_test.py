7f0b66adc987b8dc9cfb004228ec96da1182cc31
Ravi Raja <rrmerugu@gmail.com>
2016-06-26 06:40:48 +0530

    code cov tests added and setup complete

--- tests/example_test.py
+++ tests/example_test.py
@@ -5,6 +5,9 @@ class MyTestCase(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
+    def test_lower(self):
+        self.assertEqual('FOO'.lower(), 'foo')
+
 
 if __name__ == '__main__':
     unittest.main()
